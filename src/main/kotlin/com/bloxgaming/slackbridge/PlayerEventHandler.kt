7fd43b9324f9fc4e7a574e53af71a841acc419ca
Gregory Maddra <bloxgate@gmail.com>
2021-08-24 16:28:27 -0500

    Send messages on advancements

--- src/main/kotlin/com/bloxgaming/slackbridge/PlayerEventHandler.kt
+++ src/main/kotlin/com/bloxgaming/slackbridge/PlayerEventHandler.kt
@@ -1,10 +1,12 @@
 package com.bloxgaming.slackbridge
 
 import org.bukkit.Bukkit
+import org.bukkit.ChatColor
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.PlayerDeathEvent
+import org.bukkit.event.player.PlayerAdvancementDoneEvent
 import org.bukkit.plugin.Plugin
 
 object PlayerEventHandler : Listener {
@@ -14,6 +16,24 @@ object PlayerEventHandler : Listener {
         if (SlackBridge.deathMessages && SlackBridge.perms!!.has(event.entity, "slackbridge.chat.death")) {
             Bukkit.getScheduler().runTaskAsynchronously(SlackBridge.plugin as Plugin, Runnable {
                 val message = event.deathMessage ?: "${event.entity.name} died mysteriously."
+                SlackBridge.slackInterface.sendToSlackSynchronous(message)
+            })
+        }
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    fun onPlayerAdvancementDone(event: PlayerAdvancementDoneEvent) {
+        if (event.advancement.key.key.startsWith("recipes/")) {
+            return
+        }
+        if (SlackBridge.advancementMessages && SlackBridge.perms!!.has(event.player, "slackbridge.chat.advancement")) {
+            Bukkit.getScheduler().runTaskAsynchronously(SlackBridge.plugin as Plugin, Runnable {
+                val message = "${ChatColor.stripColor(event.player.displayName)} has earned the advancement [${
+                    SlackBridge.advancementTitles.getOrDefault(
+                        event.advancement.key.key,
+                        "Unrecognized Advancement (${event.advancement.key.key})]"
+                    )
+                }"
                 SlackBridge.slackInterface.sendToSlackSynchronous(message)
             })
         }
--- src/main/kotlin/com/bloxgaming/slackbridge/SlackBridge.kt
+++ src/main/kotlin/com/bloxgaming/slackbridge/SlackBridge.kt
@@ -2,7 +2,9 @@ package com.bloxgaming.slackbridge
 
 import net.milkbowl.vault.chat.Chat
 import net.milkbowl.vault.permission.Permission
+import org.bukkit.configuration.file.YamlConfiguration
 import org.bukkit.plugin.java.JavaPlugin
+import java.io.File
 
 class SlackBridge : JavaPlugin() {
 
@@ -16,6 +18,8 @@ class SlackBridge : JavaPlugin() {
         lateinit var signingSecret: String
         var connectMessages: Boolean = true
         var deathMessages: Boolean = true
+        var advancementMessages: Boolean = true
+        lateinit var advancementTitles: MutableMap<String, String>
         lateinit var bindAddress: String
         var bindPort: Int = 8888
         lateinit var slackInterface: SlackInterface
@@ -55,6 +59,7 @@ class SlackBridge : JavaPlugin() {
         config.addDefault("signing_secret", "yourSigningSecret")
         config.addDefault("send_join_and_leave", true)
         config.addDefault("send_death_messages", true)
+        config.addDefault("send_advancement_messages", true)
         config.addDefault("bind_address", "0.0.0.0")
         config.addDefault("bind_port", 8888)
         config.options().copyDefaults(true)
@@ -65,8 +70,21 @@ class SlackBridge : JavaPlugin() {
         signingSecret = config.getString("signing_secret", "yourSigningSecret")!!
         connectMessages = config.getBoolean("send_join_and_leave")
         deathMessages = config.getBoolean("send_death_messages")
+        advancementMessages = config.getBoolean("send_advancement_messages")
         bindAddress = config.getString("bind_address", "0.0.0.0")!!
         bindPort = config.getInt("bind_port", 8888)
+
+        val messagesFile = File(this.dataFolder, "messages.yml")
+        if (!messagesFile.exists()) {
+            this.saveResource("messages.yml", false)
+        }
+        val messagesConfig = YamlConfiguration.loadConfiguration(messagesFile)
+        val advancementTilesList = messagesConfig.getStringList("advancements")
+        advancementTitles = mutableMapOf()
+        advancementTilesList.forEach {
+            val parts = it.split(':', limit = 2)
+            advancementTitles[parts[0]] = parts[1]
+        }
 
         slackInterface = SlackInterface(channel, token)
         server.pluginManager.registerEvents(ChatEventHandler, this)
