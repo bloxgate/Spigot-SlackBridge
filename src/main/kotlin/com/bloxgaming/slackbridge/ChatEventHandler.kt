42201334cdaf43b9ac25c545f915e5ef476ca470
Gregory Maddra <bloxgate@gmail.com>
2021-10-03 16:34:57 -0500

    Move join and leave events from Chat to PlayerEventHandler

--- src/main/kotlin/com/bloxgaming/slackbridge/ChatEventHandler.kt
+++ src/main/kotlin/com/bloxgaming/slackbridge/ChatEventHandler.kt
@@ -6,39 +6,9 @@ import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.player.AsyncPlayerChatEvent
-import org.bukkit.event.player.PlayerJoinEvent
-import org.bukkit.event.player.PlayerQuitEvent
 import org.bukkit.plugin.Plugin
 
 object ChatEventHandler : Listener {
-
-    @EventHandler
-    fun onPlayerJoin(event: PlayerJoinEvent) {
-        val p = event.player
-        if (SlackBridge.slackConnected && SlackBridge.connectMessages && SlackBridge.perms?.has(
-                p,
-                "slackbridge.chat.join"
-            ) == true
-        ) {
-            val message = "<${ChatColor.stripColor(p.displayName)}> has joined the server"
-            Bukkit.getScheduler().runTaskAsynchronously(SlackBridge.plugin as Plugin,
-                Runnable { SlackBridge.slackInterface.sendToSlackSynchronous(message) })
-        }
-    }
-
-    @EventHandler
-    fun onPlayerQuit(event: PlayerQuitEvent) {
-        val p = event.player
-        if (SlackBridge.slackConnected && SlackBridge.connectMessages && SlackBridge.perms?.has(
-                p,
-                "slackbridge.chat.leave"
-            ) == true
-        ) {
-            val message = "<${ChatColor.stripColor(p.displayName)}> has left the server"
-            Bukkit.getScheduler().runTaskAsynchronously(SlackBridge.plugin as Plugin,
-                Runnable { SlackBridge.slackInterface.sendToSlackSynchronous(message) })
-        }
-    }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     fun onAsyncPlayerChat(event: AsyncPlayerChatEvent) {
